09c527c1c9a872743ab8b48f8eab2074e13466b9
SzychaX <kubaszyszko@onet.pl>
2025-05-06 10:40:49 +0200

    Wstępne dodatnie Eureki (do poprawek)

---
+++ eureka/src/test/java/org/inzynierka/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.inzynierka.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
