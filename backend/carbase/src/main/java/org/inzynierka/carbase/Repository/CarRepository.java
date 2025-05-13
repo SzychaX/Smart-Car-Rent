1c99d9782f09e37c6c42005746642dd5581c1420
Jakub Szyszko <86251@student.pb.edu.pl>
2025-05-13 11:39:30 +0200

    Wprowadzone poprawki, podpięcie "cars" do eureki.

--- backend/carbase/src/main/java/org/inzynierka/carbase/Repository/CarRepository.java
+++
@@ -1,7 +0,0 @@
-package org.inzynierka.carbase.Repository;
-
-import org.inzynierka.carbase.Model.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarRepository extends JpaRepository<Car, Long> {
-}
